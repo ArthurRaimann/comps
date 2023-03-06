4d25d0262ed6623866e90b9f799f84bba1e694c5
ArthurRaimann <araimann25@gmail.com>
2023-03-06 21:45:18 +0100

    bis ink.11 202

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import { useState } from "react";
-import Dropdown from "./components/Dropdown";
-import { DropdownContent } from "./models/dropdownType";
+import { useState } from 'react';
+import Dropdown from './components/Dropdown';
+import { DropdownContent } from './models/dropdownType';
 
 function App() {
-  const [selection,setSelection] = useState<DropdownContent|null>(null)
+  const [selection, setSelection] = useState<DropdownContent | null>(null);
 
   const handleSelect = (selection: DropdownContent | null) => {
-    setSelection(selection)
-  }
-  
+    setSelection(selection);
+  };
+
   const options = [
-    {label:'Red', value: 'red'},
-    {label:'Blue', value: 'blue'},
-    {label:'Green', value: 'green'},
-  ]
+    { label: 'Red', value: 'red' },
+    { label: 'Blue', value: 'blue' },
+    { label: 'Green', value: 'green' },
+  ];
 
   return (
     <div>
-      <Dropdown options={options} value={selection} onChange={handleSelect}/>
+      <Dropdown options={options} value={selection} onChange={handleSelect} />
     </div>
   );
 }
--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,32 +1,47 @@
-import { useState } from "react"
-import { DropdownType, DropdownContent } from "../models/dropdownType"
+import { useState } from 'react';
+import { GoChevronDown } from 'react-icons/go';
+import { DropdownType, DropdownContent } from '../models/dropdownType';
 
-function Dropdown({options, value, onChange}: DropdownType) {
+function Dropdown({ options, value, onChange }: DropdownType) {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const [isOpen,setIsOpen] = useState(false)
-    
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const handleClick = () => {
-        setIsOpen(!isOpen)
-    }
+  const handleOptionClick = (selectedOption: DropdownContent) => {
+    onChange(selectedOption);
+    setIsOpen(false);
+  };
 
-    const handleOptionClick = (selectedOption: DropdownContent) => {
-        onChange(selectedOption)
-        setIsOpen(false)
-    }
-
-    const optionsList = options.map((option, index) => {
-        return (
-            <div onClick={() => handleOptionClick(option)} key={`${index}${option.value}`}>{option.label}</div>
-        )
-    })
-
+  const optionsList = options.map((option, index) => {
     return (
-        <div className="border">
-            <div onClick={handleClick}>{ value?.label ||'Select...'}</div>
-            {isOpen && (<div> {optionsList}</div>)}
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+        onClick={() => handleOptionClick(option)}
+        key={`${index}${option.value}`}
+      >
+        {option.label}
+      </div>
+    );
+  });
+
+  return (
+    <div className="w-48 relative border">
+      <div
+        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+        onClick={handleClick}
+      >
+        {value?.label || 'Select...'}
+        <GoChevronDown className="text-lg" />
+      </div>
+      {isOpen && (
+        <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
+          {optionsList}
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
-export default Dropdown
+export default Dropdown;
